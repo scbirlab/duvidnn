e0db8f433f50a5ea9b1d6d803baf587420b1e3a5
Eachan Johnson <eachan.johnson@crick.ac.uk>
2025-03-25 00:13:11 +0000

    Fix JAX precision config

--- duvida/stateless/config.py
+++ duvida/stateless/config.py
@@ -19,7 +19,6 @@ class Config:
 
     def __post_init__(self):
         self.backend_installed: bool = False
-        self.set_backend(self.backend, precision=self.precision)
 
     def set_backend(self, backend: str = None, precision: str = None):
         if backend is not None:
