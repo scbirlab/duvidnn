1863be921d83638c205226990f274b2e1cc257a6
Eachan Johnson <eachan.johnson@crick.ac.uk>
2025-04-29 15:05:50 +0100

    Fix test script

--- test/scripts/train.sh
+++ test/scripts/train.sh
@@ -29,22 +29,3 @@ do
             --fp
         done
 done
-
-for class in fingerprint chemprop
-do
-    duvida train \
-        -1 hf://scbirlab/fang-2023-biogen-adme@scaffold-split:train \
-        -2 hf://scbirlab/fang-2023-biogen-adme@scaffold-split:test \
-        -S smiles \
-        -x "transformer://scbirlab/lchemme-base-zinc22-lteq300:clean_smiles~mean" \
-        -y clogp \
-        -c test/outputs/hyperopt.json \
-        -k "$class" \
-        -i 1 \
-        --prefix test/outputs/models \
-        --cache test/outputs/cache \
-        --epochs 10 \
-        --learning-rate 1e-5 \
-        --descriptors \
-        --fp
-done
